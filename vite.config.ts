81a80a00e0cc48c45af5e875c1a36a360f8c75a6
0xEbrahim <elsayedebrahim603@gmail.com>
2026-06-06 20:49:57 +0300

    update vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // Keep "/" for 0xEbrahim.github.io. Change to "/portfolio/" for a project site.
-  base: "/",
+  base: "/Ninja-portfolio/",
 });
